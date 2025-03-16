ff1b03e3ecf34955113925b906e697dc20fb0f78
Manuel <manucarrera34@gmail.com>
2025-03-16 17:59:59 +0100

    cosa noevas

---
+++ estructuras/src/main/java/org/example/AceptaElReto/Cubinfinito.java
@@ -0,0 +1,48 @@
+package org.example.AceptaElReto;
+
+import java.util.*;
+
+public class Cubinfinito {
+    static java.util.Scanner in;
+    public static boolean casoDePrueba() {
+        int numero=in.nextInt();
+        in.nextLine();
+        if (numero==0){
+            return false;
+        }
+        else {
+            String pendientes = in.nextLine();
+            String[] array = pendientes.split(" ");
+            Set<Integer> lista = new LinkedHashSet<>();
+            int pares=0;
+            int cant=0;
+
+            for (String num : array){
+                lista.add(Integer.parseInt(num));
+            }
+
+            for (Integer list : lista) {
+                for (int i = 0; i < array.length; i++) {
+                    if (Integer.parseInt(array[i])==list){
+                        cant++;
+                    }
+                }
+                if (cant>1){
+                    pares=(cant/2);
+                    if (cant%2!=0){
+                        pares--;
+                    }
+                }
+            }
+
+            System.out.println(pares);
+
+            return true;
+        }
+    }
+    public static void main(String[] args) {
+        in = new java.util.Scanner(System.in);
+        while (casoDePrueba()) {
+        }
+    }
+}
--- estructuras/src/main/java/org/example/AceptaElReto/Parentesis.java
+++ estructuras/src/main/java/org/example/AceptaElReto/Parentesis.java
@@ -1,45 +1,48 @@
 package org.example.AceptaElReto;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Parentesis {
-        static java.util.Scanner in;
+        static Scanner in;
 
         public static boolean casoDePrueba() {
             if (!in.hasNext())
                 return false;
             else {
                 String expresion = in.nextLine();
-                boolean comp;
+                int par = 0;
+                int cor = 0;
+                int lla = 0;
 
                 Stack<String> pila = new Stack<>();
-                String[] expresion_vector = expresion.split("");
 
-                for (int i = 0; i < expresion_vector.length; i++) {
-                    if (expresion_vector[i].equals("(") || expresion_vector[i].equals("{") || expresion_vector[i].equals("[")){
-                        pila.push(expresion_vector[i]);
-                    } else if (expresion_vector[i].equals(")") && pila.contains("(") || expresion_vector[i].equals("}") && pila.contains("{") || expresion_vector[i].equals("]") && pila.contains("[")) {
-                        pila.pop();
+                for (int i = 0; i < expresion.length(); i++) {
+                    pila.add(expresion.substring(i,i+1));
+                }
+
+                for (String cal : pila) {
+                    if (cal.equals("(") || cal.equals(")")){
+                        par++;
+                    } else if (cal.equals("[") || cal.equals("]")) {
+                        cor++;
+                    } else if (cal.equals("{") || cal.equals("}")) {
+                        lla++;
                     }
                 }
 
-                if (pila.isEmpty()){
-                    comp=true;
-                }else {
-                    comp=false;
-                }
-
-                if (comp){
+                if (par%2==0 && cor%2==0 && lla%2==0){
                     System.out.println("YES");
                 }else {
                     System.out.println("NO");
                 }
+
                 return true;
             }
         }
 
         public static void main(String[] args) {
-            in = new java.util.Scanner(System.in);
+            in = new Scanner(System.in);
             while (casoDePrueba()) {
             }
         }
