eb2b2a1e8aa6e8a2d1ffd5cd0b70e988de5d3789
Manuel <manucarrera34@gmail.com>
2025-03-26 19:30:53 +0100

    Doctor Muerte

--- estructuras/src/main/java/org/example/AceptaElReto/ALaCapturaDelDoctorMuerte.java
+++ estructuras/src/main/java/org/example/AceptaElReto/ALaCapturaDelDoctorMuerte.java
@@ -1,6 +1,6 @@
 package org.example.AceptaElReto;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ALaCapturaDelDoctorMuerte {
@@ -14,9 +14,30 @@ public class ALaCapturaDelDoctorMuerte {
         else {
             int numArmas = in.nextInt();
             in.nextLine();
-            List<Integer> lista_armas = new ArrayList<>();
+            List<Integer> inocentes_armas = new LinkedList<>();
+            List<Integer> villano_armas = new LinkedList<>();
             for (int i = 0; i < numArmas; i++) {
                 String dano = in.nextLine();
+                String[] array = dano.split(" ");
+                inocentes_armas.add(Integer.parseInt(array[0]));
+                villano_armas.add(Integer.parseInt(array[1]));
+            }
+            int dano_max_in=0;
+            for (Integer dano : inocentes_armas){
+                if (dano>dano_max_in){
+                    dano_max_in=dano;
+                }
+            }
+            String indices="";
+            for (int i = 0; i < villano_armas.size(); i++) {
+                if (villano_armas.get(i)>=vida && inocentes_armas.get(i)<=dano_max_in){
+                    indices=indices+i+" ";
+                }
+            }
+            if (indices.isEmpty()){
+                System.out.println("MUERTE ESCAPA");
+            }else {
+                System.out.println(indices);
             }
             return true;
         }
