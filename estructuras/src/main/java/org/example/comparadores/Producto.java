191af8a6ed955db3b177f216937a569f03c4c0c7
Manuel <manucarrera34@gmail.com>
2025-03-27 16:46:23 +0100

    aqswedrftgyhujikofdchgvhbjn

---
+++ estructuras/src/main/java/org/example/comparadores/Producto.java
@@ -0,0 +1,61 @@
+package org.example.comparadores;
+
+import java.util.Objects;
+
+public class Producto /*implements Comparable<Producto>*/{
+    private String nombre;
+    private Integer precio;
+
+    public Producto (String nombre, int precio){
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return precio == producto.precio && Objects.equals(nombre, producto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, precio);
+    }
+
+//    @Override
+//    public int compareTo(Producto producto) {
+//         int comparacionNombre = nombre.compareTo(producto.nombre);
+//
+//         if (comparacionNombre != 0){
+//             return comparacionNombre;
+//         }
+//
+//         return precio.compareTo(producto.precio);
+//
+//    }
+}
